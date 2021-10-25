9baaf390acfc88ffebd9963e094a920615de44d4
Christoph Dinh <christoph.dinh@mne-cpp.org>
2021-10-25 19:45:09 +0200

    fix include path

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ SETUP_REQUIRES += ["wheel"] if "bdist_wheel" in sys.argv else []
 
 # get the version (don't import mne here, so dependencies are not needed)
 version = None
-with open(op.join('cmb', '_version.py'), 'r') as fid:
+with open(op.join('evaler', '_version.py'), 'r') as fid:
     for line in (line.strip() for line in fid):
         if line.startswith('__version__'):
             version = line.split('=')[1].strip().strip('\'')
